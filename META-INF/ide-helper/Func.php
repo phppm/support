ae4679e8915812885c43590e4c080047719e81d5
xiaohengjin <xiaohengjin@youzan.com>
2017-07-18 20:11:05 +0800

    add meta-info

---
+++ META-INF/ide-helper/Func.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zan\Framework\Utilities\Types;
+
+
+class Func
+{
+    public static function toClosure(callable $func, $args=null, callable $validator=null)
+    {
+
+    }
+    
+    public static function call(callable $func, $args=null)
+    {
+
+    }
+
+}
---
+++ META-INF/ide-helper/Time.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Zan\Framework\Utilities\Types;
+
+class Time
+{
+    public function __construct($timeStamp=null)
+    {
+
+    }
+
+    public static function current($format=false)
+    {
+
+    }
+
+    public static function stamp()
+    {
+
+    }
+
+    public static function future($seconds,$format=false)
+    {
+
+    }
+
+    public static function past($seconds,$format=false)
+    {
+
+    }
+
+    public static function phpDiff($t1, $t2 = false, $format = '%y-%m-%d %h:%i:%s', $sign=false)
+    {
+
+    }
+
+    /**
+     *  计算两个时间差值
+     */
+    public static function diff($t1, $t2 = false, $format = 'a', $sign = false)
+    {
+
+    }
+}
